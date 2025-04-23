b1fe085a1df5dc90a2b3f3d4ffc67c41a67c8145
amer-common-wealth <amer@common-wealth.io>
2025-04-23 21:49:35 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -98,8 +98,7 @@ const Connectors = () => {
                 console.log('connector2', connector2)
 
                 if (connector2.id === 'phantom' && !isInPhantomApp) { 
-                  const phantomDeepLink = `https://phantom.app/ul/v1/connect?app_url=${encodeURIComponent(document.location.href)}&dapp_encryption_public_key=${'12345667890'}&redirect_link=${encodeURIComponent(document.location.href)}`
-                  //const phantomDeepLink = `https://phantom.app/ul/browse/${encodeURIComponent(document.location.href)}?ref=${encodeURIComponent(document.location.href)}`
+                  const phantomDeepLink = `https://phantom.app/ul/browse/${encodeURIComponent(document.location.href)}?ref=${encodeURIComponent(document.location.href)}`
                   window.location.href = phantomDeepLink
                   return
                  }
